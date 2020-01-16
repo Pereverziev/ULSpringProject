af0d7833832ca63b93efd023e4f4d8217aae8902
Pereverziev <volodymyr.pereverziev@gmail.com>
2020-01-16 22:34:39 +0100

    s

--- src/main/java/bot/services/Webhook.java
+++ src/main/java/bot/services/Webhook.java
@@ -7,8 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Controller
 public class Webhook {
+
+    @PostConstruct
+    public void init() {
+        LOGGER.info("hello world");
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Webhook.class);
 
