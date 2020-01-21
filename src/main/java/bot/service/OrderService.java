a30480f6bd83ac34838210fd615a78ecfa8aa641
Pereverziev <volodymyr.pereverziev@gmail.com>
2020-01-21 14:20:49 +0100

    fix

--- src/main/java/bot/service/OrderService.java
+++ src/main/java/bot/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.Map;
 public class OrderService {
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
     private static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
-    private static final BigDecimal NINETEEN_NINE = new BigDecimal(99);
+    private static final BigDecimal NINETEEN_NINE_FIVE = new BigDecimal(99.5);
 
     @Autowired
     private BinanceApiMarginRestClient marginClient;
@@ -42,7 +42,7 @@ public class OrderService {
     public void closePositionIfOneExists(String assetPairTimeframe) {
         final NewOrderResponse position = assetPairTimeframeToOpenPositionMap.get(assetPairTimeframe);
         if (position != null) {
-            final BigDecimal returnAmount = new BigDecimal(position.getExecutedQty());
+            final BigDecimal returnAmount = new BigDecimal(position.getExecutedQty()).multiply(NINETEEN_NINE_FIVE).divide(ONE_HUNDRED, RoundingMode.DOWN).round(new MathContext(assetService.getRounding(position.getSymbol()), RoundingMode.DOWN));
             final NewOrder newOrder = new NewOrder(position.getSymbol(), position.getSide().equals(OrderSide.BUY) ? OrderSide.SELL : OrderSide.BUY, OrderType.MARKET, null, returnAmount.toPlainString());
             LOGGER.info("Closing position " + position);
             final NewOrderResponse newOrderResponse = marginClient.newOrder(newOrder);
@@ -50,7 +50,7 @@ public class OrderService {
 //                final BigDecimal loan = new BigDecimal(newOrderResponse.getExecutedQty()).multiply(assetService.getLastPriceOfAssetPair(position.getSymbol())).multiply(NINETEEN_NINE).divide(ONE_HUNDRED, RoundingMode.DOWN);
                 borrowService.repayAsset("USDT", "29");
             } else {
-                final BigDecimal loan = new BigDecimal(newOrderResponse.getExecutedQty()).multiply(NINETEEN_NINE).divide(ONE_HUNDRED, RoundingMode.DOWN).round(new MathContext(assetService.getRounding(position.getSymbol()), RoundingMode.DOWN));
+                final BigDecimal loan = new BigDecimal(newOrderResponse.getExecutedQty()).multiply(NINETEEN_NINE_FIVE).divide(ONE_HUNDRED, RoundingMode.DOWN).round(new MathContext(assetService.getRounding(position.getSymbol()), RoundingMode.DOWN));
                 borrowService.repayAsset(position.getSymbol().subSequence(0, position.getSymbol().length() - 4).toString(), loan.toPlainString());
             }
             assetPairTimeframeToOpenPositionMap.remove(assetPairTimeframe);
