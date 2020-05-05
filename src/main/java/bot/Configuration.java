e57df8964dde17aa3b46ed33425618ff7d009c6c
Pereverziev <volodymyr.pereverziev@gmail.com>
2020-05-05 18:36:56 +0200

    commit

--- src/main/java/bot/Configuration.java
+++ src/main/java/bot/Configuration.java
@@ -1,29 +1,11 @@
 package bot;
 
-import com.binance.api.client.BinanceApiClientFactory;
-import com.binance.api.client.BinanceApiMarginRestClient;
-import com.binance.api.client.BinanceApiRestClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Configuration {
 
-    @Value("${apiKey}")
-    private String apiKey;
-    @Value("${secret}")
-    private String secret;
 
-    @Bean
-    public BinanceApiMarginRestClient binanceApiMarginRestClient() {
-        return BinanceApiClientFactory.newInstance(apiKey, secret).newMarginRestClient();
-    }
-
-    @Bean
-    public BinanceApiRestClient binanceApiRestClient() {
-        return BinanceApiClientFactory.newInstance(apiKey, secret).newRestClient();
-    }
 }
